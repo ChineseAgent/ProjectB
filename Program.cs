fafad94f22b3613ac7547231692a43fc1f0c418e
TymovanRijn <1057297@hr.nl>
2023-03-21 23:31:06 +0100

    Inlogscherm branch

--- Program.cs
+++ Program.cs
@@ -8,21 +8,23 @@ public class Program
 {
     public static void Main()
     {
-        Menu A = new Menu();
-        A.snel();
-        TableView.CreateTables();
-        while (true)
-        {
-            Console.Clear();
-            // Send.SendReservationConfirmation("tymovanrijn@gmail.com", "23-04-2023", "18:00-20:00").Wait();
-            Console.WriteLine("Met hoeveel personenen bent u?");
-            int AantalPersonen = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-            TableView.ReserveerTafel(AantalPersonen);
+        // Menu A = new Menu();
+        // A.snel();
+        // TableView.CreateTables();
+        // while (true)
+        // {
+        //     Console.Clear();
+        //     // Send.SendReservationConfirmation("tymovanrijn@gmail.com", "23-04-2023", "18:00-20:00").Wait();
+        //     Console.WriteLine("Met hoeveel personenen bent u?");
+        //     int AantalPersonen = Convert.ToInt32(Console.ReadLine());
+        //     Console.Clear();
+        //     TableView.ReserveerTafel(AantalPersonen);
 
-            TableView.Tafelindeling();
-            Thread.Sleep(5000);
-        }
+        //     TableView.Tafelindeling();
+        //     Thread.Sleep(5000);
+        // }
+
+        Inlogscherm.Keuzemenu();
 
     }
 }
