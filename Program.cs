968fe9c294a3d732895fb354a7d3420bbf3410f5
SemLindeboom <1051855@hr.nl>
2023-03-27 09:23:57 +0200

    Error uitgewerkt

--- Program.cs
+++ Program.cs
@@ -11,6 +11,12 @@ public class Program
         TableView.CreateTables();
         while (true)
         {
+        Menu A = new Menu();
+        // A.snel();
+        TableView.CreateTables();
+        while (true)
+        {
+            Console.Clear();
             // Send.SendReservationConfirmation("tymovanrijn@gmail.com", "23-04-2023", "18:00-20:00").Wait();
             Console.WriteLine("Met hoeveel personenen bent u?");
             int AantalPersonen = Convert.ToInt32(Console.ReadLine());
@@ -18,6 +24,11 @@ public class Program
             TableView.ReserveerTafel(AantalPersonen);
 
             TableView.Tafelindeling();
+            Thread.Sleep(5000);
+
+            // load_data();
+        }
+
         }
 
     }
